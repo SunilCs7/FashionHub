dc255ea351f05dd653fdb776d75ab332ce92385a
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-12-02 12:14:00 +0530

    All folder sTructure of app, and routing,bavbar design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import LoginSignup from "./Pages/LoginSignup";
+import Product from "./Pages/Product";
+import Shop from "./Pages/Shop";
+import ShopCat from "./Pages/ShopCat";
+import Cart from "./Pages/Cart";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use BrowserRouter to wrap your routes
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          {" "}
+          <Route path="/" element={<Shop />} /> {/* Default route */}
+          {/* Routes for Shop Categories */}
+          <Route path="/mens" element={<ShopCat category="men" />} />
+          <Route path="/womens" element={<ShopCat category="women" />} />
+          <Route path="/kids" element={<ShopCat category="kid" />} />
+          {/* Product route with dynamic productId */}
+          <Route path="/product" element={<Product />}>
+            <Route path=":productid" element={<Product />} />
+          </Route>
+          {/* Login route */}
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/ShopCat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopCat = () => {
+  return (
+      <div>
+          <h1>Shop Categories</h1>
+    </div>
+  )
+}
+
+export default ShopCat
